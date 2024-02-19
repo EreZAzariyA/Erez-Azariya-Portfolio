8701660d1cf800c0f1d3f8453d57355bb73dd9d8
Erez Azariya <erezazariya@gmail.com>
2024-02-19 23:48:19 +0200

    ballCanvas motion.div added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,20 @@
 import { technologies } from "../constants";
 import { SectionWrapper } from "../hoc";
+import { fadeIn } from "../utils/motion";
 import { BallCanvas } from "./canvas";
-
+import { motion } from "framer-motion";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <motion.div variants={fadeIn('left', "spring", 1, 1.5)}>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </motion.div>
   );
 };
 
